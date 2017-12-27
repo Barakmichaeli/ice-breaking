bc1dbf3aa361682bfbe65f5b4cb07f7620e9dafe
Barakmichaeli <barak6546@gmail.com>
2017-12-27 16:21:14 +0200

    Structure was build

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var game = require('./routes/game');
 var app = express();
 
 // view engine setup
@@ -12,33 +9,58 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const COOKIE_NAME = "DatingGames";
 // uncomment after placing your favicon in /public
-var num = 1;
-var obj = {};
-
+var roomsCounter = 1;
+var rooms = {};
 
 //Redirect
 app.get('/createRoom', function (req, res, next) {
-    obj[num] = "OK";
-    console.log(obj[num]);
-    res.redirect(302, "/room/" + (num++));
+    rooms[roomsCounter] = {'counter': 0};
+    res.redirect(302, "/room/" + (roomsCounter++));
 });
 
 /* GET new rooms */
-app.get("/room/:num", function (req, res, next){
-    var reqNum = req.params.num;
-    if (obj[reqNum]){
-        req.socket
-        res.sendFile(path.join(__dirname, '/views', 'room.html'));
+app.get("/room/:num", function (req, res, next) {
+    var room = req.params.num;
+    //No room OR room full
+    if (!rooms[room] || rooms[room]['counter'] > 1) {
+        res.redirect('/');
     }
-    else
-        res.send(400,"Room number doesn't exists");
+
+    //One of the opponents reconnect
+    // console.log(req.cookies);
+    if (!isRegistered(req.cookies, room)) {
+        console.log("here");
+        //register new member
+        var counter = ++rooms[room]['counter']; //opponent k
+        rooms[room]['opponent' + counter] = counter; //opponentk : k
+        res.cookie(COOKIE_NAME, counter, {
+            expires: new Date(Date.now() + 900000),
+            path: '/room/' + room,
+            httpOnly: true
+        });
+    }
+    res.sendFile(path.join(__dirname, '/views', 'room.html'));
+});
+
+
+function isRegistered(cookies, room) {
+    var uid = parseInt(cookies[COOKIE_NAME]);
+    return (uid && (uid === rooms[room]['opponent1']
+        || uid === rooms[room]['opponent2']));
+}
+
+
+app.get("/dissconnect",function(req,res,next){
+
+
 });
 
 
@@ -54,7 +76,6 @@ app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500);
     res.render('error');
