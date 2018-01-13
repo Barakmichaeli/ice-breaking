e8377fd4b63662a7acca3663de6de6cdb9ad9193
Barakmichaeli <barak6546@gmail.com>
2018-01-13 19:07:19 +0200

    change port

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
+
+
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000, function () {
+server.listen(8080, function () {
     console.log('listening on *:3000');
 });
 
-//DONT WORK
-io.sockets.on('connection', function (socket) {
-    console.log("sd");
-});
 
 /**
  * External modules
